f7f8a5c8b09c5a478a472c8bcdabfddcc3b4897b
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-09 12:46:11 -0300

    feat: add functionality that allows users to get a poll data through endpoint route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
 
+import { getPoll } from './routes/get-poll'
 import { createPoll } from './routes/create-poll'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server is running at localhost:3333!')
