d0b70e7409292bbb288cc77c288e7ac49c120f04
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-09 12:52:36 -0300

    feat: add functionality that allows users to vote on a poll option

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify'
 
 import { getPoll } from './routes/get-poll'
 import { createPoll } from './routes/create-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
 
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server is running at localhost:3333!')
