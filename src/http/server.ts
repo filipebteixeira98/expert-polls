7938b12823ec678db8c393f4445c5725eb4f336a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-11 18:09:05 -0300

    chore: configure websocket protocol

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 
 import { getPoll } from './routes/get-poll'
 import { createPoll } from './routes/create-poll'
@@ -11,6 +12,8 @@ app.register(cookie, {
   secret: 'polls-app',
   hook: 'onRequest',
 })
+
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
