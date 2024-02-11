80a54dd2787e74b728c8a7bfe34ce7d3305442e8
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-11 17:41:55 -0300

    feat: add functionality that allows users to see how many votes each poll options has

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -25,6 +26,34 @@ export async function getPoll(app: FastifyInstance) {
       },
     })
 
-    return reply.send({ poll })
+    if (!poll) {
+      return reply.status(400).send({ message: 'Poll not found!' })
+    }
+
+    const votesResult = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const formattedVotes = votesResult.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = votesResult[index + 1]
+
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in formattedVotes ? formattedVotes[option.id] : 0,
+          }
+        }),
+      },
+    })
   })
 }
